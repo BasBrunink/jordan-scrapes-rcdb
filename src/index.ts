f03e56a5185df6fb583d94981c2c82de4d3b549f
Jordan Hansen <jhansen1@lenovo.com>
2020-05-19 10:22:24 -0600

    fix for updated html structure and add a user-agent to the request

--- src/index.ts
+++ src/index.ts
@@ -1,102 +1,117 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import cheerio from 'cheerio';
 import * as json2csv from 'json2csv';
 import * as fs from 'fs';
 
 (async () => {
-	const domain = 'https://rcdb.com';
-	const region = 'r.htm?ol=1&ot=2';
+    const domain = 'https://rcdb.com';
+    const region = 'r.htm?ol=1&ot=2';
 
-	const axiosResponse = await axios.get(`${domain}/${region}`);
+    const axiosResponse = await axios.get(`${domain}/${region}`, {
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'
+        }
+    });
 
-	const $ = cheerio.load(axiosResponse.data);
+    const $ = cheerio.load(axiosResponse.data);
 
-	const totalRollerCoasters = parseInt($('.int').text());
-	// Assume 24 per page
-	const totalPages = Math.ceil(totalRollerCoasters / 24);
+    const totalRollerCoasters = parseInt($('.int').text());
+    // Assume 24 per page
+    const totalPages = Math.ceil(totalRollerCoasters / 24);
 
-	console.log('total pages', totalPages);
+    console.log('total pages', totalPages);
 
-	const rollerCoasters: any[] = [];
-	for (let page = 1; page < totalPages; page++) {
-		const axiosResponsePaginated = await axios.get(`${domain}/${region}&page=${page}`);
-		const paginated$ = cheerio.load(axiosResponsePaginated.data);
-		const rows = paginated$('#report tbody tr');
+    const rollerCoasters: any[] = [];
+    for (let page = 1; page < totalPages; page++) {
+        const axiosResponsePaginated = await axios.get(`${domain}/${region}&page=${page}`, {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'
+            }
+        });
+        const paginated$ = cheerio.load(axiosResponsePaginated.data);
+        const rows = paginated$('.stdtbl tbody tr');
 
-		for (let i = 0; i < rows.length; i++) {
-			const row$ = cheerio.load(rows[i]);
-			const link = row$('td:nth-of-type(2) a').attr('href');
+        for (let i = 0; i < rows.length; i++) {
+            const row$ = cheerio.load(rows[i]);
+            const link = row$('td:nth-of-type(2) a').attr('href');
 
-			if (link) {
-				const rollerCoaster = await getDetails(`${domain}${link}`);
-				console.log('link', link, rollerCoaster);
-				rollerCoasters.push(rollerCoaster);
-			}
-		}
-	}
-	const csv = json2csv.parse(rollerCoasters);
+            if (link) {
+                const rollerCoaster = await getDetails(`${domain}${link}`);
+                console.log('link', link, rollerCoaster);
+                rollerCoasters.push(rollerCoaster);
+            }
 
-	fs.writeFile('rollerCoasters.csv', csv, async (err) => {
-		if (err) {
-			console.log('err while saving file', err);
-		}
-	});
+            await timeout(1000);
+        }
+    }
+    const csv = json2csv.parse(rollerCoasters);
+
+    fs.writeFile('rollerCoasters.csv', csv, async (err) => {
+        if (err) {
+            console.log('err while saving file', err);
+        }
+    });
 
 })();
 
 export async function getDetails(detailsLink: string) {
 
-	const axiosResponse = await axios.get(detailsLink);
+    const axiosResponse = await axios.get(detailsLink, {
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'
+        }
+    });
 
-	const $ = cheerio.load(axiosResponse.data);
-
-
-	const rollerCoaster: any = {
-		name: $('#feature h1').text(),
-		parkName: $('#feature .scroll:nth-of-type(1) a:nth-of-type(1)').text(),
-		city: $('#feature .scroll:nth-of-type(1) a:nth-of-type(2)').text(),
-		state: $('#feature .scroll:nth-of-type(1) a:nth-of-type(3)').text(),
-		country: $('#feature .scroll:nth-of-type(1) a:nth-of-type(4)').text(),
-		link: detailsLink,
-		make: $('#feature .scroll:nth-of-type(2) a:nth-of-type(1)').text(),
-		model: $('#feature .scroll:nth-of-type(2) a:nth-of-type(2)').text()
-	};
-
-	const featuredHtml = $('#feature').html();
-	const operatingInfoHtml$ = cheerio.load(`<div>${featuredHtml.split('<br>')[2]}</div>`);
-	const operatingMess = operatingInfoHtml$('div').text();
-	if (operatingMess.toLowerCase().includes('removed')) {
-		rollerCoaster.active = false;
-		rollerCoaster.started = operatingInfoHtml$('div time:nth-of-type(1)').attr('datetime');
-		rollerCoaster.ended = operatingInfoHtml$('div time:nth-of-type(2)').attr('datetime');
-	}
-	else {
-		rollerCoaster.active = true;
-		rollerCoaster.started = operatingInfoHtml$('div time:nth-of-type(1)').attr('datetime');
-	}
-
-	// console.log('featured stuff', operatingInfoHtml$('div').text(), `<div>${featuredHtml.split('<br>')[2]}</div>`);
-
-	const rows = $('#statTable tr');
-	for (let i = 0; i < rows.length; i++) {
-		const row$ = cheerio.load(rows[i]);
-		if (row$('th').text().toLowerCase() === 'elements') {
-			const elements = row$('td a');
-			const elementsToPush: any[] = [];
-			for (let elementIndex = 0; elementIndex < elements.length; elementIndex++) {
-				elementsToPush.push(cheerio.load(elements[elementIndex])('a').text());
-			}
-
-			rollerCoaster[row$('th').text().toLowerCase()] = elementsToPush.join(', ');
-		}
-		else {
-			rollerCoaster[row$('th').text().toLowerCase()] = row$('td').text();
-		}
-
-	}
-
-	return Promise.resolve(rollerCoaster);
+    const $ = cheerio.load(axiosResponse.data);
 
 
+    const rollerCoaster: any = {
+        name: $('#feature h1').text(),
+        parkName: $('#feature > div > a:nth-of-type(1)').text(),
+        city: $('#feature > div > a:nth-of-type(2)').text(),
+        state: $('#feature > div > a:nth-of-type(3)').text(),
+        country: $('#feature > div > a:nth-of-type(4)').text(),
+        link: detailsLink,
+        make: $('#feature .scroll:nth-of-type(2) a:nth-of-type(1)').text(),
+        model: $('#feature .scroll:nth-of-type(2) a:nth-of-type(2)').text()
+    };
 
+    const featuredHtml = $('#feature').html();
+    const operatingInfoHtml$ = cheerio.load(`<div>${featuredHtml.split('<br>')[2]}</div>`);
+    const operatingMess = operatingInfoHtml$('div').text();
+    if (operatingMess.toLowerCase().includes('removed')) {
+        rollerCoaster.active = false;
+        rollerCoaster.started = operatingInfoHtml$('div time:nth-of-type(1)').attr('datetime');
+        rollerCoaster.ended = operatingInfoHtml$('div time:nth-of-type(2)').attr('datetime');
+    }
+    else {
+        rollerCoaster.active = true;
+        rollerCoaster.started = operatingInfoHtml$('div time:nth-of-type(1)').attr('datetime');
+    }
+
+    // console.log('featured stuff', operatingInfoHtml$('div').text(), `<div>${featuredHtml.split('<br>')[2]}</div>`);
+
+    const rows = $('#statTable tr');
+    for (let i = 0; i < rows.length; i++) {
+        const row$ = cheerio.load(rows[i]);
+        if (row$('th').text().toLowerCase() === 'elements') {
+            const elements = row$('td a');
+            const elementsToPush: any[] = [];
+            for (let elementIndex = 0; elementIndex < elements.length; elementIndex++) {
+                elementsToPush.push(cheerio.load(elements[elementIndex])('a').text());
+            }
+
+            rollerCoaster[row$('th').text().toLowerCase()] = elementsToPush.join(', ');
+        }
+        else {
+            rollerCoaster[row$('th').text().toLowerCase()] = row$('td').text();
+        }
+
+    }
+
+    return Promise.resolve(rollerCoaster);
+}
+
+function timeout(ms: number) {
+	return new Promise(res => setTimeout(res, ms));
 }
